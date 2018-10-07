9afda7721c617d2d7dee5443ca991cfadf6113d0
DmitryLutskovich <43153697+DmitryLutskovich@users.noreply.github.com>
2018-10-07 19:30:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ module.exports = function longestConsecutiveLength(array) {
 	let length = 0;
 	let current = 0;
 	array.sort((a,b) => a-b);
-	if(array.length<2) return array.length;
+	if(array.length<2)
+	{
+		return array.length;
+	}
 	array.forEach(function (item, i, array)
 		      {
 		if(item === array[i+1]-1)
